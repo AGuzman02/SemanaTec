1a5266b30cfa6637e70d31a0d3316051f713e572
karlajacome08 <a00833141@itesm.mx>
2023-09-14 22:52:47 -0600

    front-pag principal

--- SemanaTec/frontend/src/App.js
+++ SemanaTec/frontend/src/App.js
@@ -3,7 +3,7 @@ import Navbar from './components/Navbar/Navbar';
 import fondo from './components/Imagenes/fondo.png'
 import '../src/App.css'
 import BackgroundImage from './components/BackroundImage';
-import { BrowserRouter as Router, Route, Switch, Routes } from 'react-router-dom';
+import { Navigate,BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './components/Home';
 import AgregarProducto from './components/Agregar Productos/Agregar_Producto';
 import Lista_Carro from './components/Carrito/Lista_Carro';
@@ -17,15 +17,15 @@ function App() {
     <div className="App">
       
       <BackgroundImage imageUrl={imageUrl}>
-        <Router> {/* Envuelve toda la aplicación con el Router */}
+        <Router> 
           <Navbar />
-            <Routes> {/* Utiliza Routes para renderizar solo una ruta a la vez */}
+            <Routes> 
+              <Route path="/" element={<Home />} />
               <Route path="/Home" element={<Home />} />
               <Route path="/productos_disponibles" element={<Barra_busqueda />} />
               <Route path="/agregar_productos" element={<AgregarProducto />} />
               <Route path="/carrito" element={<Lista_Carro />} />
               
-              {/* Agrega más rutas según sea necesario */}
             </Routes>
         </Router>
         
